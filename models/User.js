2b25a1ec2cfe25f0286209e20ea9a60421f2b976
Kieran <60425579+Memphis1983@users.noreply.github.com>
2021-04-27 15:10:42 +0530

    added comments

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+// setting up our User Schema, each user will have a microsoftID and displayName associated with their login account, which is saved in our DB
 const UserSchema = new mongoose.Schema({
   microsoftId: {
     type: String,
