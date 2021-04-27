8d2fc71ba47ac2ca02c206b7fbbaf6df42fe6544
Kieran <60425579+Memphis1983@users.noreply.github.com>
2021-04-27 11:16:51 +0530

    Added comments

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
+// Importing mongoose package
 const mongoose = require('mongoose')
 
+// setting up a constant to connect to our database with the DB_STRING
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
@@ -7,12 +9,14 @@ const connectDB = async () => {
       useUnifiedTopology: true,
       useFindAndModify: false,
     })
-
+// confirm connection if successful 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
+// throw error if anything goes wrong or not connected
   } catch (err) {
     console.error(err)
+// process.exit instructs node.js to terminate the process synchronously an exit status code
     process.exit(1)
   }
 }
-
+// export this function to re-use and import elsewhere
 module.exports = connectDB
