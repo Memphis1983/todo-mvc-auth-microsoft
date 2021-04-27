8f80fed3e27145310e80fb5b06f0ef4bb7add753
Kieran <60425579+Memphis1983@users.noreply.github.com>
2021-04-27 15:07:10 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,5 @@
+//The the authentication is cleared user gets to todos page or back to home/login page
+
 module.exports = {
     ensureAuth: function (req, res, next) {
       if (req.isAuthenticated()) {
