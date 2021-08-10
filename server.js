3ad796b025314942780c5c9aed7412f06229cf1d
Memphis1983 <kiran.iyer83@gmail.com>
2021-08-10 17:43:39 +0530

    deploy-test-heroku

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-// import all necessary node packages
-const express = require('express') //node framework which provides features for web applications 
-const app = express() // creates a new express server 
-const mongoose = require('mongoose') // connects to our DB
-const passport = require('passport') // authentication middleware that helps add authenticaton (username and password)
-const session = require('express-session') // creates a session middleware, Session data is not saved in the cookie itself, just the session ID. Session data is stored server-side.
-const MongoStore = require('connect-mongo')(session) //MongoDB session store for Connect and Express written in Typescript
-const connectDB = require('./config/database') //Our DB connection config imported from config/config.js
-const authRoutes = require('./routes/auth') // Our authentication routes setup in routes/auth
-const homeRoutes = require('./routes/home')  // Our home/get routes 
-const todoRoutes = require('./routes/todos') // out get/post/put/delete routes
+// !import all necessary node packages
+
+//node framework which provides features for web applications 
+const express = require('express') 
+
+// creates a new express server 
+const app = express() 
+
+// connects to our DB
+const mongoose = require('mongoose') 
+
+// authentication middleware that helps add authenticaton (username and password)
+const passport = require('passport') 
+
+ // creates a session middleware, Session data is not saved in the cookie itself, just the session ID. Session data is stored server-side.
+const session = require('express-session')
+
+//MongoDB session store for Connect and Express written in Typescript
+const MongoStore = require('connect-mongo')(session) 
+
+//Our DB connection config imported from config/config.js
+const connectDB = require('./config/database') 
+
+// Our authentication routes setup in routes/auth
+const authRoutes = require('./routes/auth') 
+
+// Our home/get routes 
+const homeRoutes = require('./routes/home')  
+
+// Our get/post/put/delete routes
+const todoRoutes = require('./routes/todos') 
 
 // initializing our environment variables from the mentioned path 
 require('dotenv').config({path: './config/.env'})
@@ -20,7 +40,7 @@ require('./config/passport')(passport)
 // connecting to our DB (function is being called)
 connectDB()
 
-// add & configure middleware:
+// ! add & configure middleware:
 // This defines where to look for our view files and configures the view engine as ejs
 app.set('view engine', 'ejs')
 // Serving static files, Express, by default does not allow you to serve static files. You need to enable it using the following built-in middleware.
@@ -30,7 +50,7 @@ app.use(express.urlencoded({ extended: true }))
 //  also can parse data like text and json
 app.use(express.json())
 
-// Sessions | I DONT UNDERSTAND THIS 
+// !Sessions 
 app.use(
     session({
       secret: 'keyboard cat',
@@ -41,16 +61,16 @@ app.use(
   )
 
   
-// Passport middleware | Each app.use(middleware) is called every time a request is sent to the server.
+// !Passport middleware | Each app.use(middleware) is called every time a request is sent to the server.
 app.use(passport.initialize())
 app.use(passport.session())
 
-// initializing routes from routes/ path
+// !initializing routes from routes/ path
 app.use('/', homeRoutes)
 app.use('/auth', authRoutes)
 app.use('/todos', todoRoutes)
 
-// Server running  
+// !Server running  
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
